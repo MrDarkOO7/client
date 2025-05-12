e049a53df2c183af693c74edcc241bba6f77c713
MrDarkOO7 <m.waleedapsacian@gmail.com>
2025-05-12 19:15:53 +0500

    RTK setup

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Navbar from './(components)/Navbar'
 import Sidebar from './(components)/Sidebar'
+import StoreProvider, { useAppSelector } from './redux'
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSideBarCollapsed = useAppSelector((state)=>state.global.isSideBarCollapsed)
+  const isDarkMode = useAppSelector((state)=> state.global.isDarkMode);
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add("dark");
+    }else{
+      document.documentElement.classList.remove("dark");
+    }
+  })
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* Sidebar */}
       <Sidebar />
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSideBarCollapsed? "" : "md:pl-64"}`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  )
+}
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return(
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   )
 }
 
